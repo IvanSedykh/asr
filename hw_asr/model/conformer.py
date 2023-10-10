3133e74c3cf42ead7ac3ee5ca8fbcfcdc2593a85
Ivan Sedykh <sed.ivan.dm@gmail.com>
2023-10-10 18:20:09 +0300

    RoPE + custom mhsa

--- hw_asr/model/conformer.py
+++ hw_asr/model/conformer.py
@@ -1,5 +1,10 @@
+import math
+from typing import Optional
+
 import torch
 from torch import nn, Tensor
+import torch.nn.functional as F
+from rotary_embedding_torch import RotaryEmbedding
 
 from hw_asr.base import BaseModel
 
@@ -99,20 +104,111 @@ class FeedForwardModule(nn.Module):
         return x + 0.5 * self.block(x)
 
 
+# heavily inspired by opensource conformer
+# modified to use RoPE instead of relative attn -- we live in 2023
+class RelativeMultiHeadAttention(nn.Module):
+    """
+    Multi-head attention with RoPE.
+    This concept was proposed in the "RoFormer"
+
+    Args:
+        d_model (int): The dimension of model
+        num_heads (int): The number of attention heads.
+        dropout_p (float): probability of dropout
+
+    Inputs: query, key, value, pos_embedding, mask
+        - **query** (batch, time, dim): Tensor containing query vector
+        - **key** (batch, time, dim): Tensor containing key vector
+        - **value** (batch, time, dim): Tensor containing value vector
+        - **mask** (batch, 1, time2) or (batch, time1, time2): Tensor containing indices to be masked
+
+    Returns:
+        - **outputs**: Tensor produces by relative multi head attention module.
+    """
+
+    def __init__(
+        self,
+        d_model: int,
+        num_heads: int,
+        dropout_p: float = 0.0,
+        learned_freq_rope=False,
+    ):
+        super().__init__()
+        assert d_model % num_heads == 0, "d_model % num_heads should be zero."
+        self.d_model = d_model
+        self.d_head = int(d_model / num_heads)
+        self.num_heads = num_heads
+        self.sqrt_dim = math.sqrt(d_model)
+
+        self.query_proj = nn.Linear(d_model, d_model)
+        self.key_proj = nn.Linear(d_model, d_model)
+        self.value_proj = nn.Linear(d_model, d_model)
+
+        # todo fix learned_freq=True
+        self.rotary_emb = RotaryEmbedding(
+            dim=self.d_head, learned_freq=learned_freq_rope
+        )
+
+        self.dropout = nn.Dropout(p=dropout_p)
+
+        self.out_proj = nn.Linear(d_model, d_model)
+
+    def forward(
+        self,
+        query: Tensor,
+        key: Tensor,
+        value: Tensor,
+    ) -> Tensor:
+        batch_size = value.size(0)
+
+        query = self.query_proj(query).view(batch_size, -1, self.num_heads, self.d_head)
+        key = (
+            self.key_proj(key)
+            .view(batch_size, -1, self.num_heads, self.d_head)
+            .permute(0, 2, 1, 3)
+        )
+        value = (
+            self.value_proj(value)
+            .view(batch_size, -1, self.num_heads, self.d_head)
+            .permute(0, 2, 1, 3)
+        )
+
+        # apply rope
+        query = self.rotary_emb.rotate_queries_or_keys(query)
+        key = self.rotary_emb.rotate_queries_or_keys(key)
+
+        content_score = torch.matmul((query).transpose(1, 2), key.transpose(2, 3))
+        score = content_score / self.sqrt_dim
+
+        attn = F.softmax(score, -1)
+        attn = self.dropout(attn)
+
+        context = torch.matmul(attn, value).transpose(1, 2)
+        context = context.contiguous().view(batch_size, -1, self.d_model)
+
+        return self.out_proj(context)
+
+
 class MHSAModule(nn.Module):
-    def __init__(self, dim, num_heads, dropout_prob=0.0):
+    def __init__(self, dim, num_heads, dropout_prob=0.0, learned_freq_rope=False):
         super().__init__()
 
         # todo: add positional embeddings
         self.layernorm = nn.LayerNorm(dim)
-        self.attn = nn.MultiheadAttention(
-            dim, num_heads, dropout_prob, batch_first=True
+        # self.attn = nn.MultiheadAttention(
+        #     dim, num_heads, dropout_prob, batch_first=True, need_weights=False
+        # )
+        self.attn = RelativeMultiHeadAttention(
+            d_model=dim,
+            num_heads=num_heads,
+            dropout_p=dropout_prob,
+            learned_freq_rope=learned_freq_rope,
         )
         self.dropout = nn.Dropout(dropout_prob)
 
     def _residual(self, x):
         x = self.layernorm(x)
-        x, attn_weights = self.attn(x, x, x)
+        x = self.attn(x, x, x)
         x = self.dropout(x)
         return x
 
@@ -122,12 +218,23 @@ class MHSAModule(nn.Module):
 
 class ConformerBlock(nn.Module):
     def __init__(
-        self, dim, hidden_dim=None, dropout_p=0.0, num_heads=4, kernel_size=31
+        self,
+        dim,
+        hidden_dim=None,
+        dropout_p=0.0,
+        num_heads=4,
+        kernel_size=31,
+        learned_freq_rope=False,
     ):
         super().__init__()
         self.block = nn.Sequential(
             FeedForwardModule(dim, hidden_dim=hidden_dim, dropout_prob=dropout_p),
-            MHSAModule(dim, num_heads=num_heads, dropout_prob=dropout_p),
+            MHSAModule(
+                dim,
+                num_heads=num_heads,
+                dropout_prob=dropout_p,
+                learned_freq_rope=learned_freq_rope,
+            ),
             ConvModule(dim, kernel_size=kernel_size, dropout_p=dropout_p),
             FeedForwardModule(dim, hidden_dim=hidden_dim, dropout_prob=dropout_p),
             nn.LayerNorm(dim),
@@ -195,6 +302,7 @@ class Conformer(BaseModel):
         kernel_size=31,
         num_blocks=8,
         do_conv_subsampling=False,
+        learned_freq_rope=False,
         **batch
     ):
         super().__init__(n_feats, n_class, **batch)
@@ -213,6 +321,7 @@ class Conformer(BaseModel):
                     dropout_p=dropout_p,
                     num_heads=num_heads,
                     kernel_size=kernel_size,
+                    learned_freq_rope=learned_freq_rope,
                 )
                 for _ in range(num_blocks)
             ]
